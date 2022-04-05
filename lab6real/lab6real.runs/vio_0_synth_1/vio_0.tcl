008e793b49dc64a5b704aeecd38b0d6c23482779
Drew Bodmer <bodmer.d@northeastern.edu>
2022-04-05 15:06:17 -0400

    lab 7

--- lab6real/lab6real.runs/vio_0_synth_1/vio_0.tcl
+++ lab6real/lab6real.runs/vio_0_synth_1/vio_0.tcl
@@ -70,6 +70,8 @@ proc create_report { reportName command } {
   }
 }
 OPTRACE "vio_0_synth_1" START { ROLLUP_AUTO }
+set_param chipscope.maxJobs 4
+set_param xicom.use_bs_reader 1
 set_param project.vivado.isBlockSynthRun true
 set_msg_config -msgmgr_mode ooc_run
 OPTRACE "Creating in-memory project" START { }
@@ -87,9 +89,9 @@ set_property ip_output_repo c:/Users/bodmer.d/labs/lab6real/lab6real.cache/ip [c
 set_property ip_cache_permissions {read write} [current_project]
 OPTRACE "Creating in-memory project" END { }
 OPTRACE "Adding files" START { }
-read_ip -quiet C:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0/vio_0.xci
-set_property used_in_implementation false [get_files -all c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0/vio_0.xdc]
-set_property used_in_implementation false [get_files -all c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0/vio_0_ooc.xdc]
+read_ip -quiet c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0_1/vio_0.xci
+set_property used_in_implementation false [get_files -all c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0_1/vio_0.xdc]
+set_property used_in_implementation false [get_files -all c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0_1/vio_0_ooc.xdc]
 
 OPTRACE "Adding files" END { }
 # Mark all dcp files as not used in implementation to prevent them from being
@@ -155,32 +157,32 @@ create_report "vio_0_synth_1_synth_report_utilization_0" "report_utilization -fi
 OPTRACE "synth reports" END { }
 
 if { [catch {
-  file copy -force C:/Users/bodmer.d/labs/lab6real/lab6real.runs/vio_0_synth_1/vio_0.dcp c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0/vio_0.dcp
+  file copy -force C:/Users/bodmer.d/labs/lab6real/lab6real.runs/vio_0_synth_1/vio_0.dcp c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0_1/vio_0.dcp
 } _RESULT ] } { 
   send_msg_id runtcl-3 error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
   error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
 }
 
 if { [catch {
-  write_verilog -force -mode synth_stub c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0/vio_0_stub.v
+  write_verilog -force -mode synth_stub c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0_1/vio_0_stub.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a Verilog synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_vhdl -force -mode synth_stub c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0/vio_0_stub.vhdl
+  write_vhdl -force -mode synth_stub c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0_1/vio_0_stub.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a VHDL synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_verilog -force -mode funcsim c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0/vio_0_sim_netlist.v
+  write_verilog -force -mode funcsim c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0_1/vio_0_sim_netlist.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the Verilog functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_vhdl -force -mode funcsim c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0/vio_0_sim_netlist.vhdl
+  write_vhdl -force -mode funcsim c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0_1/vio_0_sim_netlist.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the VHDL functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
@@ -190,32 +192,32 @@ if { [catch {
 
 
 if { [catch {
-  file copy -force C:/Users/bodmer.d/labs/lab6real/lab6real.runs/vio_0_synth_1/vio_0.dcp c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0/vio_0.dcp
+  file copy -force C:/Users/bodmer.d/labs/lab6real/lab6real.runs/vio_0_synth_1/vio_0.dcp c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0_1/vio_0.dcp
 } _RESULT ] } { 
   send_msg_id runtcl-3 error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
   error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
 }
 
 if { [catch {
-  file rename -force C:/Users/bodmer.d/labs/lab6real/lab6real.runs/vio_0_synth_1/vio_0_stub.v c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0/vio_0_stub.v
+  file rename -force C:/Users/bodmer.d/labs/lab6real/lab6real.runs/vio_0_synth_1/vio_0_stub.v c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0_1/vio_0_stub.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a Verilog synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/Users/bodmer.d/labs/lab6real/lab6real.runs/vio_0_synth_1/vio_0_stub.vhdl c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0/vio_0_stub.vhdl
+  file rename -force C:/Users/bodmer.d/labs/lab6real/lab6real.runs/vio_0_synth_1/vio_0_stub.vhdl c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0_1/vio_0_stub.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a VHDL synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/Users/bodmer.d/labs/lab6real/lab6real.runs/vio_0_synth_1/vio_0_sim_netlist.v c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0/vio_0_sim_netlist.v
+  file rename -force C:/Users/bodmer.d/labs/lab6real/lab6real.runs/vio_0_synth_1/vio_0_sim_netlist.v c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0_1/vio_0_sim_netlist.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the Verilog functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/Users/bodmer.d/labs/lab6real/lab6real.runs/vio_0_synth_1/vio_0_sim_netlist.vhdl c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0/vio_0_sim_netlist.vhdl
+  file rename -force C:/Users/bodmer.d/labs/lab6real/lab6real.runs/vio_0_synth_1/vio_0_sim_netlist.vhdl c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0_1/vio_0_sim_netlist.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the VHDL functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
@@ -224,13 +226,13 @@ if { [catch {
 
 if {[file isdir C:/Users/bodmer.d/labs/lab6real/lab6real.ip_user_files/ip/vio_0]} {
   catch { 
-    file copy -force c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0/vio_0_stub.v C:/Users/bodmer.d/labs/lab6real/lab6real.ip_user_files/ip/vio_0
+    file copy -force c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0_1/vio_0_stub.v C:/Users/bodmer.d/labs/lab6real/lab6real.ip_user_files/ip/vio_0
   }
 }
 
 if {[file isdir C:/Users/bodmer.d/labs/lab6real/lab6real.ip_user_files/ip/vio_0]} {
   catch { 
-    file copy -force c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0/vio_0_stub.vhdl C:/Users/bodmer.d/labs/lab6real/lab6real.ip_user_files/ip/vio_0
+    file copy -force c:/Users/bodmer.d/labs/lab6real/lab6real.srcs/sources_1/ip/vio_0_1/vio_0_stub.vhdl C:/Users/bodmer.d/labs/lab6real/lab6real.ip_user_files/ip/vio_0
   }
 }
 file delete __synthesis_is_running__
---
+++ lab6real/lab6real.srcs/sources_1/new/pcreg.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/05/2022 01:55:28 PM
+// Design Name: 
+// Module Name: pcreg
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pcreg(input pb_clk_debounced, input right_pb_rst_general, output reg [7:0] pc);
+    //reg[7:0] pcinternal;
+    always @(posedge pb_clk_debounced, posedge right_pb_rst_general) begin
+        if (right_pb_rst_general)
+            pc = 8'b00000000;
+        else
+            pc = pc + 1;
+    end
+    
+endmodule
